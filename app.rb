8697d20731272d4d75135777621253ba11298bc9
funavocado6787 <158098423+funavocado6787@users.noreply.github.com>
2024-07-22 18:34:43 +0300

    Updated footer and header

---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+require 'sinatra/base'
+require './lib/player.rb'
+require './lib/game.rb'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+  get '/' do 
+   erb :index
+  end
+
+  post '/names' do
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
+    redirect '/play'
+  end
+  
+  get '/play' do
+    @game = $game
+    erb :play
+  end
+  
+  get '/attack' do
+    @game = $game
+    @game.attack(@game.player_2)
+    @game.switch_turns
+    erb :attack
+  end
+  run! if app_file == $0
+end
+
